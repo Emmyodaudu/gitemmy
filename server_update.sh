5aa375423941d50ccac4e2d929dd9897b1bd402a
ElaigwuMichael <michaelelaigwu@nvit.tech>
2023-01-16 03:55:40 -0800

    My commit message

---
+++ server_update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+install_run_start_apache() {
+
+#update
+sudo apt-get update
+
+# install Apache server
+sudo apt-get install apache2 -y
+
+# start Apache server
+sudo service apache2 start
+
+# check the status of Apache server
+sudo service apache2 status
+}
+
+install_run_start_apache
